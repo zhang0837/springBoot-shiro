70da102bd2586ca9dc2ae8d9fc9f34062915231c
王诗林 <399038956@qq.com>
2020-09-20 20:56:29 +0800

    添加用户信息不存在异常处理

--- spring-shiro-demo/src/main/java/com/wsl/controller/LoginController.java
+++ spring-shiro-demo/src/main/java/com/wsl/controller/LoginController.java
@@ -4,6 +4,7 @@ import com.wsl.bean.User;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationException;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
@@ -29,12 +30,14 @@ public class LoginController {
             subject.login(usernamePasswordToken);
 //            subject.checkRole("admin");
 //            subject.checkPermissions("query", "add");
+        } catch (UnknownAccountException e) {
+            log.error("用户名不存在！", e);
+            return "用户名不存在！";
         } catch (AuthenticationException e) {
             log.error("账号或密码错误！", e);
             return "账号或密码错误！";
         } catch (AuthorizationException e) {
             log.error("没有权限！", e);
-            e.printStackTrace();
             return "没有权限";
         }
         return "login success";
